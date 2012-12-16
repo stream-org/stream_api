1214a753643a5d0f86c271a8678110e3d544277a
John Gesimondo <john@jmondo.com>
2012-12-16 12:54:03 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stream::Application.config.secret_token = 'fe43501e3f4043f52f21e96821525ed4746121770fa446697f8e3507d851d5003889363c7731d3c313d205146c734f867e5c35fb0bf48eac673bdcea208d61e9'
