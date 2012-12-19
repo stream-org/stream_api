4ba1ba613c53efc7afc9e9d73f520ab9892f8228
John Gesimondo <john@jmondo.com>
2012-12-18 23:32:32 -0800

    remove some todos :)

--- app/controllers/api/user_streams_controller.rb
+++ app/controllers/api/user_streams_controller.rb
@@ -5,7 +5,6 @@ class Api::UserStreamsController < ApplicationController
     @streams = Stream.where(owner_id: params[:user_id])
 
     # returns 200 with array of user objects (or empty array)
-    # TODO: add # participants, # of pics, and most recent pic methods to response
     respond_to do |format|
       format.json { render json: @streams.to_json(:methods => [:participants_count, :pictures_count, :last_picture]) }
     end
