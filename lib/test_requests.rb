0bc4acb303319c32612e9bf4fab95f886c6f7184
John Gesimondo <john@jmondo.com>
2012-12-18 23:00:21 -0800

    run all test requests at once

--- lib/test_requests.rb
+++ lib/test_requests.rb
@@ -1,11 +1,16 @@
 require 'httparty'
 require 'json'
 
-# response = HTTParty.post('http://localhost:3000/api/users/sign_in.json', body: { user: { phone: '550053321', password: 'password'}})
-# response = HTTParty.post('http://localhost:3000/api/users/sign_up.json', body: { user: { first: 'john', last: 'g', phone: '5550053321', password: 'asdhfashdf'}})
-# response = HTTParty.get( 'http://localhost:3000/api/streams/5.json')
-# response = HTTParty.post('http://localhost:3000/api/streams.json', body: { stream: { name: 'The best stream!', owner_id: 1}})
-# response = HTTParty.get( 'http://localhost:3000/api/users/1/streams.json')
-response = HTTParty.get( 'http://localhost:3000/api/pictures/1.json')
-p response.code
-p response.parsed_response
+responses = [
+  HTTParty.post('http://localhost:3000/api/users/sign_in.json', body: { user: { phone: '550053321', password: 'password'}}),
+  HTTParty.post('http://localhost:3000/api/users/sign_up.json', body: { user: { first: 'john', last: 'g', phone: '5550053321', password: 'asdhfashdf'}}),
+  HTTParty.get( 'http://localhost:3000/api/streams/5.json'),
+  HTTParty.post('http://localhost:3000/api/streams.json', body: { stream: { name: 'The best stream!', owner_id: 1}}),
+  HTTParty.get( 'http://localhost:3000/api/users/1/streams.json'),
+  HTTParty.get( 'http://localhost:3000/api/pictures/1.json')
+]
+
+responses.each do |r|
+  p r.code
+  p r.parsed_response
+end
