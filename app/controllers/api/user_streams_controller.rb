e5e1c5ea91fb7b3993f5a41b727c35290d4f038e
John Gesimondo <john@jmondo.com>
2012-12-18 23:31:29 -0800

    add participants count, picture count, and last picture to user streams api, stream model

--- app/controllers/api/user_streams_controller.rb
+++ app/controllers/api/user_streams_controller.rb
@@ -7,7 +7,7 @@ class Api::UserStreamsController < ApplicationController
     # returns 200 with array of user objects (or empty array)
     # TODO: add # participants, # of pics, and most recent pic methods to response
     respond_to do |format|
-      format.json { render json: @streams }
+      format.json { render json: @streams.to_json(:methods => [:participants_count, :pictures_count, :last_picture]) }
     end
   end
 
--- app/models/stream.rb
+++ app/models/stream.rb
@@ -2,4 +2,17 @@ class Stream < ActiveRecord::Base
   attr_accessible :name, :owner_id
   belongs_to :owner, :class_name => User
   has_many :pictures
+
+  # TODO: use sql counter cache
+  delegate :count, to: :pictures, prefix: true
+
+  def participants_count
+    # TODO: add participants count here (probably use sql counter cache)
+    0
+  end
+
+  def last_picture
+    pictures.last
+  end
+
 end
